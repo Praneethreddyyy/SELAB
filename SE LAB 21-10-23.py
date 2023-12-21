17affa7a0e5e37a8e3d10c12c2e8b69d8e1a8e67
Praneethreddyyy <153796356+Praneethreddyyy@users.noreply.github.com>
2023-12-21 10:37:34 +0530

    Create SE LAB 21-10-23.py

---
+++ SE LAB 21-10-23.py
@@ -0,0 +1,12 @@
+--variable_as_static_code--
+def weather_prediction (t,h,w):
+  w=(0.5*t**2-0.2*h+0.1*w-15)
+  if w>300:
+    return "sunny",w
+  elif 200<w<=300:
+    return "cloudy",w
+  elif 100<w<=200:
+    return "rainy",w
+  else 
+    return "stormy",w
+print(weather_prediction(20,70,15)

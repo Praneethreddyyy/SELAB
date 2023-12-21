bf6f581daf246da07dbbc34c80920b3dfb522c20
Praneethreddyyy <153796356+Praneethreddyyy@users.noreply.github.com>
2023-12-21 10:51:26 +0530

    Update SE LAB 21-10-23.py

--- SE LAB 21-10-23.py
+++ SE LAB 21-10-23.py
@@ -1,12 +1,12 @@
 --variable_as_static_code--
 def weather_prediction (t,h,w):
-  w=(0.5*t*t-0.2*h+0.1*w-15)
-  if w>300:
-    return "sunny",w
-  elif 200<w<=300:
-    return "cloudy",w
-  elif 100<w<=200:
-    return "rainy",w
+  W=0.5*t*t-0.2*h+0.1*w-15
+  if W>300:
+    return "sunny",W
+  elif 200<W<=300:
+    return "cloudy",W
+  elif 100<W<=200:
+    return "rainy",W
   else 
-    return "stormy",w
+    return "stormy",W
 print(weather_prediction(20,70,15)
